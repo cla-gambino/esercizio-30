0a82d4aa8e82063ce706c286c081f5df496661d5
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-01-16 19:14:08 +0100

    commit update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Car myCar = new Car("1.3", "AA 000 AA", "Fiat", "Panda");
+        System.out.println("la macchina è: " + myCar.getBrand() + " " + myCar.getModel() + " " + myCar.getDisplacement() + " targata: " + myCar.getPlate());
+    }
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+class Car {
+    String displacement, plate, brand, model;
+
+    public Car(java.lang.String displacement, java.lang.String plate, java.lang.String brand, java.lang.String model) {
+        this.displacement = displacement;
+        this.plate = plate;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public java.lang.String getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(java.lang.String displacement) {
+        this.displacement = displacement;
+    }
+
+    public java.lang.String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(java.lang.String plate) {
+        this.plate = plate;
+    }
+
+    public java.lang.String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(java.lang.String brand) {
+        this.brand = brand;
+    }
+
+    public java.lang.String getModel() {
+        return model;
+    }
+
+    public void setModel(java.lang.String model) {
+        this.model = model;
     }
 }
